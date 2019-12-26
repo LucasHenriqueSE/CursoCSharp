691b82c3c364052d5d1926cc8d31c67af33ba594
Lucas Henrique <lucas.sousa@outlook.com>
2019-12-26 18:28:19 -0300

    Commit inicial

---
+++ CursoCSharp/Fundamentos/PrimeiroPrograma.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CursoCSharp.Fundamentos {
+    class PrimeiroPrograma {
+        public static void Executar() {
+            /* Escreve uma nova linha de texto no
+             * console e mantém o ponto de inserção na mesma linha
+             */
+            Console.Write("Primeiro ");
+
+            /* Escreve uma nova linha de texto no
+             * console e coloca o ponto de inserção na próxima linha
+             */
+            Console.WriteLine("Programa");
+            Console.WriteLine("Terminou!");
+        }
+    }
+}
